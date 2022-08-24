40e71e62d11ad8474a397df405f5099db0a5c11b
Guga Gongadze <guga@uiflow.com>
2022-08-24 16:53:13 +0400

    fix: ex 1.7

--- exercise_1.7.js
+++ exercise_1.7.js
@@ -1,3 +1,10 @@
 const { sqrt } = require('./utils');
 
-console.log(sqrt(9));
+console.log(sqrt(0.00000001));
+console.log(Math.sqrt(0.00000001));
+// For very small numbers `is_good_enough` becomes inadequate
+// for its use of 0.001 for checking how good a guess is.
+
+console.log(sqrt(300000000000000));
+console.log(Math.sqrt(300000000000000));
+// For very large numbers we encounter a stack overflow
--- utils.js
+++ utils.js
@@ -1,14 +1,44 @@
-const is_good_enough = (guess, x) => abs(square(guess) - x) < 0.001;
-const square = (x) => x * x;
-const abs = (x) => (x >= 0 ? x : -x);
-const improve = (guess, x) => average(guess, x / guess);
-const average = (x, y) => x + y / 2;
-const sqrt_iter = (guess, x) =>
-  is_good_enough(guess, x) ? guess : sqrt_iter(improve(guess, x), x);
-const sqrt = (x) => sqrt_iter(1, x);
+function is_good_enough(guess, x) {
+  return abs(square(guess) - x) < 0.001;
+}
+
+function better_is_good_enough(previous_guess, new_guess, x) {
+  if (abs((new_guess - previous_guess) / new_guess) < 0.00000000001) return true;
+
+  return false;
+}
+
+function square(x) {
+  return x * x;
+}
+
+function abs(x) {
+  return x >= 0 ? x : -x;
+}
+
+function improve(guess, x) {
+  return average(guess, x / guess);
+}
+
+function average(x, y) {
+  return (x + y) / 2;
+}
+
+function sqrt_iter(previous_guess, new_guess, x) {
+  if (better_is_good_enough(previous_guess, new_guess, x)) return new_guess;
+
+  const improved_guess = improve(new_guess, x);
+
+  return sqrt_iter(new_guess, improved_guess, x);
+}
+
+function sqrt(x) {
+  return sqrt_iter(undefined, 1, x);
+}
 
 module.exports = {
   is_good_enough,
+  better_is_good_enough,
   square,
   abs,
   improve,
